791900bc68d631eac767004d855f7daceaa2960e
mac <ibtisam.tanveer22@gmail.com>
2025-02-20 03:08:13 +0100

    migration concept

--- asiatouragency/models.py
+++ asiatouragency/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    # we need origin country destination night price
+    origin_country = models.CharField(max_length=50)
+    destination_country = models.CharField(max_length=50)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
